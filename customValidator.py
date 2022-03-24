bed7177603971bf9b3676460d08703de99c7c527
santosh177gt@gmail.com <67412675+KarmaTsring@users.noreply.github.com>
2022-03-24 09:39:56 +0545

    Updated with cerberus validator with two dict, prototype

--- customValidator.py
+++ customValidator.py
@@ -1,22 +1,57 @@
+import re
+from cerberus import Validator 
 
-
-def validators(dict):
-    validations = {
-    "name": lambda x: isinstance(x, str) and 5 <= len(x) <= 45,
-    "lat": lambda x: isinstance(x, float) and -90 <= x <= 90,
-    "lng": lambda x: isinstance(x, float) and -180 <= x <= 180,
-    }
-    for k,v in dict.items():
-        # return validations.get(keys, lambda x: False)(values)
-        return all(validations.get(k, lambda x: False)(v) for (k, v) in dict.items())
-    
-
-def check(dict, func):
-    return func(dict)
-
-dicts = {'name':'Seewew',
-        'lat':43.222,
-        'lng':-22.3331
+schl_dicts = { 
+        
+                #'name':'Exqwqwq', 'address':'Swayambhu','sch_type':'Higher Secondary School',
+                'name':'Koteshwor', 'address':'Buddha Park','sch_type':'Higher Secondary School'
+                
         }
 
-print(check(dicts, validators))
+hsp_dicts = {
+                #'name':'Manmohan Hospital','hsp_id': 22, 'address':'Sitapaila',
+                'name':'Norvic International','hsp_id': 1002,'address':'Thapathali'
+                }
+
+
+def schl_validators(schl_dict):
+    schema = {
+        'name':{'type':'string', 'minlength':5, 'maxlength':25},
+        'sch_id':{'type':'integer', 'min':1, 'max':300},
+        'address':{'type':'string', 'min':5, 'max':50},
+        'sch_type':{'type':'string', 'min':5, 'max':90}
+         }
+        
+    v = Validator(schema)
+    if v.validate(schl_dict):
+        return True
+    else:
+        return False
+
+
+def hsp_validators(hsp_dict):
+    schema = {
+        'name':{'type':'string', 'minlength':7, 'maxlength':30},
+        'hsp_id':{'type':'integer', 'min':1, 'max':400},
+        'address':{'type':'string', 'min':5, 'max':50}
+         }
+
+    v = Validator(schema)
+    if v.validate(hsp_dict):
+        return True
+    else:
+        return False
+
+
+
+#function as argument for school dict, validator 
+def schl_check(schl_dict, func):
+    return func(schl_dict)
+
+#function as argument for hospital dict, validator
+def hsp_check(hsp_dict, func):
+    return func(hsp_dict)
+
+
+print(f'School Dict valid: {schl_check(schl_dicts, schl_validators)}')
+print(f'Hospital dict is Valid: {hsp_check(hsp_dicts, hsp_validators)}')   
